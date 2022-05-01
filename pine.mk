94430f15a2a29725d1cfbdeba6f2fbc5ae6ce417
me-cafebabe <me_cafebabe@hotmail.com>
2022-05-01 16:57:32 +0800

    import audio and sensor configs from stock

--- pine.mk
+++ pine.mk
@@ -2,6 +2,15 @@ $(call inherit-product, device/xiaomi/sdm439-common/msm8937_64.mk)
 
 DEVICE_PACKAGE_OVERLAYS := device/xiaomi/pine/overlay
 
+# Audio
+PRODUCT_COPY_FILES += \
+    device/xiaomi/pine/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    device/xiaomi/pine/audio/mixer_paths_sdm439_pm8953.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_sdm439_pm8953.xml
+
+# Sensors
+PRODUCT_COPY_FILES += \
+    device/xiaomi/pine/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/pine/pine-vendor.mk)
 
