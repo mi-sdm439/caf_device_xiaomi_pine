54fb7d7bc6582cbc419ea578fc804f070d7ec3fb
me-cafebabe <me_cafebabe@hotmail.com>
2022-05-02 04:14:31 +0800

    build audio_aw87329_audio.ko

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,3 +3,6 @@ TARGET_AW87329 := true
 
 # Inherit from common sdm439-common
 include device/xiaomi/sdm439-common/BoardConfig.mk
+
+BOARD_VENDOR_KERNEL_MODULES += \
+    $(KERNEL_MODULES_OUT)/audio_aw87329_audio.ko
--- pine.mk
+++ pine.mk
@@ -7,6 +7,8 @@ PRODUCT_COPY_FILES += \
     device/xiaomi/pine/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     device/xiaomi/pine/audio/mixer_paths_sdm439_pm8953.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_sdm439_pm8953.xml
 
+PRODUCT_PACKAGES += audio_aw87329_audio.ko
+
 # Sensors
 PRODUCT_COPY_FILES += \
     device/xiaomi/pine/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
