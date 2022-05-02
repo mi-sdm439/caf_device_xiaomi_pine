cd1b6a9cd5d892bbab232afb1fe4cba90f209ed8
me-cafebabe <me_cafebabe@hotmail.com>
2022-05-02 21:34:47 +0800

    FACTORY_VERSION_MODE := true
    used by audio-kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,6 @@
 KERNEL_DEFCONFIG := pine-perf_defconfig
 TARGET_AW87329 := true
+FACTORY_VERSION_MODE := true
 
 # Inherit from common sdm439-common
 include device/xiaomi/sdm439-common/BoardConfig.mk
